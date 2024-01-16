0709699b980e0045cf906474df71f325eedd58ba
Victor <josvictoronofre@gmail.com>
2024-01-16 12:23:27 -0300

    Project creation and Position class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,26 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position() {}
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    
+    public Integer getRow() {
+        return row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
