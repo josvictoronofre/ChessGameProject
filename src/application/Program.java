f843de5827357a89454523501867e04a2338616b
Victor <josvictoronofre@gmail.com>
2024-01-26 15:13:01 -0300

    implementing pawn promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,11 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
+                if (match.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    match.replacePromotedPiece(type);
+                }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
